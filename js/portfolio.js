f95ffeacd9976a594d912b45a9737daa91609d8c
jhayslett <jhayslett@gmail.com>
2017-11-29 00:13:47 -0700

    Add portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,27 @@
+function galleryHeaders(){
+	var allProjects = document.querySelector(".projects");
+	var eachProject = allProjects.querySelectorAll(".project");
+	for(var a = 0; a < eachProject.length; a++) {
+		addGalleryHeaderLinks(eachProject[a]);
+	}
+}
+
+function addGalleryHeaderLinks(e){
+	var header = e.querySelector("header");
+	header.addEventListener("click",function(){
+		if(this.parentElement.classList.contains("active") === true) {
+			this.parentElement.classList.remove("active");
+			this.parentElement.classList.add("inactive");
+		} else {
+			this.parentElement.classList.remove("inactive");
+			this.parentElement.classList.add("active");
+		}
+	})
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+	console.log("Loaded");
+
+	galleryHeaders();
+});
